2141f90a6326d9a5e314b654a6b8c96ecceee60a
Gabriel Gamon <younggamon15@gmail.com>
2023-08-18 22:01:44 -0300

    Update Files

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ const btn_cadastrar =  document.querySelector('#btn-ca')
 const btn_relatorio = document.querySelector('#btn-re')
 
 btn_cadastrar.addEventListener('click', ()=>{
-    location.href = "cadastro.php";
+    location.href = "pages/register.html";
 })
 
 btn_relatorio.addEventListener('click', () => {
-    location.href = 'relatorio.html'
+    location.href = 'pages/report.html'
 })
